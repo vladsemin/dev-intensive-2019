31edc0b232ea773ea3b51521b3a12c90984169d7
Vladislav <v.semin@skbkontur.ru>
2019-07-10 05:12:26 +0000

    all tasks

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -17,4 +17,61 @@ object Utils {
         val secondInit = lastName.orEmpty().trim().getOrNull(0)?.toUpperCase()?.toString() ?: ""
         return "$firstInit$secondInit".ifEmpty { null }
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val dictionary = getDictionary()
+        val stringBuilder = StringBuilder()
+
+        for (char in payload.trim())
+            stringBuilder.append(replaceCharacter(char, dictionary))
+
+        return stringBuilder.toString().replace(" ", divider)
+    }
+
+    private fun replaceCharacter(char: Char, dictionary: HashMap<Char, String>): String {
+        val transl  = dictionary[char.toLowerCase()] ?: char.toString()
+
+        return if (char.isUpperCase() && transl.isNotEmpty())
+            transl.capitalize()
+        else transl
+    }
+
+    private fun getDictionary(): HashMap<Char, String> {
+        val hashMap = hashMapOf<Char, String>()
+        hashMap['а'] = "a"
+        hashMap['б'] = "b"
+        hashMap['в'] = "v"
+        hashMap['г'] = "g"
+        hashMap['д'] = "d"
+        hashMap['е'] = "e"
+        hashMap['ё'] = "e"
+        hashMap['ж'] = "zh"
+        hashMap['з'] = "z"
+        hashMap['и'] = "i"
+        hashMap['й'] = "i"
+        hashMap['к'] = "k"
+        hashMap['л'] = "l"
+        hashMap['м'] = "m"
+        hashMap['н'] = "n"
+        hashMap['о'] = "o"
+        hashMap['п'] = "p"
+        hashMap['р'] = "r"
+        hashMap['с'] = "s"
+        hashMap['т'] = "t"
+        hashMap['у'] = "u"
+        hashMap['ф'] = "f"
+        hashMap['х'] = "h"
+        hashMap['ц'] = "c"
+        hashMap['ч'] = "ch"
+        hashMap['ш'] = "sh"
+        hashMap['щ'] = "sh'"
+        hashMap['ъ'] = ""
+        hashMap['ы'] = "i"
+        hashMap['ь'] = ""
+        hashMap['э'] = "e"
+        hashMap['ю'] = "yu"
+        hashMap['я'] = "ya"
+
+        return hashMap
+    }
 }
