2b7395c84187869c18adb0c467d9a5fefe979a44
Vladislav <v.semin@skbkontur.ru>
2019-07-10 04:14:07 +0000

    tasks 1..6, 8..12

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,3 +4,9 @@ fun String.truncate(length: Int = 16): String{
     val trimmedString = this.trim()
     return if (trimmedString.length <= length) trimmedString else trimmedString.substring(0, length).trim() + "..."
 }
+
+fun String.stripHtml(): String{
+    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
+    val spaceRegex = Regex(" {2,}")
+    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
+}
